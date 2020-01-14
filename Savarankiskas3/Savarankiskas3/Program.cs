310ccd60a66c118194f132bc45bcf374a7196225
Vilija <Simonaviciene@users.noreply.github.com>
2020-01-14 21:17:53 +0200

    veikia

---
+++ Savarankiskas3/Savarankiskas3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Savarankiskas3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string eilute = IsvestiSimboliuEilute("abc", "#");
+            Console.WriteLine(eilute);
+            Console.ReadLine();
+           
+        }
+        private static string IsvestiSimboliuEilute(string tekstas, string simbolis)
+        {
+            string rezultatas = "";
+            for (int i = 0; i < tekstas.Length; i++)
+            {
+                rezultatas += simbolis;
+
+            }
+            return rezultatas;
+        }
+
+
+
+
+    }
+}
