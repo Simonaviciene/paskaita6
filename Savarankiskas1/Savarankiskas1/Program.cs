1425042b00b01461aa6eb9c9c393ea81d8b6fdc9
Vilija <Simonaviciene@users.noreply.github.com>
2020-01-14 20:29:53 +0200

    veikia

---
+++ Savarankiskas1/Savarankiskas1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Savarankiskas1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string vardas = "Vilija";
+        
+            IsvedaManoVarda(vardas);
+            Console.ReadLine();
+        }
+
+        private static void IsvedaManoVarda(string v)
+        {
+            Console.WriteLine(v);
+
+        }
+
+
+
+    }
+}
---
+++ Savarankiskas2/Savarankiskas2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Savarankiskas2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int kartojamasVardas = 5;
+                string vardas = "Vilija";
+
+                IsvedaManoVarda(vardas, kartojamasVardas);
+                Console.ReadLine();
+            }
+
+            private static void IsvedaManoVarda(string vardas, int kartojamasVardas)
+            {
+                for (int i = 0; i < kartojamasVardas; i++)
+                {
+                     Console.WriteLine(vardas);
+                }
+               
+
+            }
+        }
+    }
+}
