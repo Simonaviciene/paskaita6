669b94c599958ce5c9181b3aefe5a0cd07867d74
Vilija <Simonaviciene@users.noreply.github.com>
2020-01-16 10:11:29 +0200

    masyvo elementai

---
+++ Savarankiskas2.2/Savarankiskas2.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Savarankiskas2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] skaiciai = { 10, 5, 6, 8, 4, 2 };
+
+            IsvestiElementuMasyva(skaiciai);
+            Console.ReadLine();
+        }
+        static void IsvestiElementuMasyva(int[] skaiciai)
+        {
+            int skaiciuSuma = 0;
+
+            for (int i = 0; i < skaiciai.Length; i++)
+            {
+                skaiciuSuma += skaiciai[i];
+            }
+            Console.WriteLine($"Suma: {skaiciuSuma}, viso: {skaiciai.Length} elementai");
+
+        }
+    }
+}
