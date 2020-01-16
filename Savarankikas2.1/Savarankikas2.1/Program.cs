04efeea550d499402cffce4fbd19f3a22dbc37bc
Vilija <Simonaviciene@users.noreply.github.com>
2020-01-16 09:32:28 +0200

    suskaiciuoja tarpus

---
+++ Savarankikas2.1/Savarankikas2.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Savarankikas2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tekstas = "Ir vel isauso nauja diena !";
+            
+
+            Console.WriteLine(SuskaiciuotiTarpusTekste(tekstas));
+            Console.ReadLine();
+
+        }
+        static int SuskaiciuotiTarpusTekste(string tekstas)
+        {
+            int tarpai = 0;
+
+            foreach(var item in tekstas)
+            {
+                if(item == ' ')
+                {
+                    tarpai ++;
+                }
+            }
+            return tarpai;
+        }
+
+            
+    }
+}
