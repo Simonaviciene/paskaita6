60a5f57c8bae356b53b02cf087a84609621d4bb8
Vilija <Simonaviciene@users.noreply.github.com>
2020-01-16 09:13:35 +0200

    KMI

---
+++ Savarankiskas4/Savarankiskas4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Savarankiskas4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double ugis = 1.70;
+            double mase = 51;
+
+            Console.WriteLine(SuskaiciuojaKMI(ugis, mase));
+            Console.ReadLine();
+            
+        }
+        static double SuskaiciuojaKMI(double ugis, double mase)
+        {
+            double kmi = mase / Math.Pow(ugis, 2);
+            return kmi;
+            
+        }
+
+    }
+}
