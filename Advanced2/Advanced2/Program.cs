ad386333a2372da2d7580217a81b3b0df167adb8
Vilija <Simonaviciene@users.noreply.github.com>
2020-01-19 16:37:18 +0200

    Fibonacio veikia

---
+++ Advanced2/Advanced2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced2
+{
+    class Program
+    {
+        public static int Fibonacio(int n)
+        {
+            int x = 0;
+            int a = 1;
+           
+            for (int i = 0; i < n; i++)
+            {
+                int temp = x;
+                x = a;
+                a = temp + a;
+            }
+            return x;
+        }
+
+        static void Main()
+        {
+            for (int i = 0; i < 14; i++)
+            {
+                Console.WriteLine(Fibonacio(i));
+              
+            }
+            Console.ReadLine();
+        }  
+    }
+}
